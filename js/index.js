d21cda6ff5fb97031a234c548d1948b6cedc4098
dfreeman3102 <davidfreeman3102@gmail.com>
2024-03-31 20:17:53 -0400

    added additional prompt and basic structure of html

--- js/index.js
+++ js/index.js
@@ -3,37 +3,42 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 
 inquirer
- .prompt([
+  .prompt([
     {
-        type:"input",
-        name:"name",
-        message:"What is your name?"
+      type: "input",
+      name: "name",
+      message: "What is your name?",
     },
     {
-        type:"input",
-        name:"location",
-        message:"Where are you from?"
+        type:"number",
+        name:"age",
+        message:"How old are you?"
     },
     {
-        type:"input",
-        name:"bio",
-        message:"Tell me something about yourself."
+      type: "input",
+      name: "location",
+      message: "Where are you from?",
     },
     {
-        type:"input",
-        name:"linkedIn",
-        message:"What is your LinkedIn URL?"
+      type: "input",
+      name: "bio",
+      message: "Tell me something about yourself.",
     },
     {
-        type:"input",
-        name:"gitHub",
-        message:"What is your GitHub URL?"
+      type: "input",
+      name: "linkedIn",
+      message: "What is your LinkedIn URL?",
     },
- ])
- .then((data) => {
-    const filename = `${data.name.toLowerCase().split(' ').join('')}.json`;
+    {
+      type: "input",
+      name: "gitHub",
+      message: "What is your GitHub URL?",
+    },
+  ])
+  .then((data) => {
+    const filename = `${data.name.toLowerCase().split(" ").join("")}.json`;
 
-    fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
-      err ? console.log(err) : console.log('Success!')
+    fs.writeFile(filename, JSON.stringify(data, null, "\t"), (err) =>
+      err ? console.log(err) : console.log("Success!")
     );
   });
